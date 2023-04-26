9ca06ddc533c9be4c7df0997bee0d9b52991f3fa
Henrik Kirk <henrikkirk@gmail.com>
2023-04-26 15:13:31 +0200

    Initial commit

---
+++ Akka1.5.3Test/Program.fs
@@ -0,0 +1,18 @@
+﻿open Akka
+open Akka.Actor.Dsl
+open Akka.FSharp
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+let helloActor msg =
+    match msg with
+    | "hello" -> printfn "Hello to you"
+    | _ -> printfn "Wrong message"
+
+
+let system = System.create "NewSystem" (Configuration.load())
+
+let actor = spawn system "Actor" (actorOf helloActor)
+
+actor <! "hello"
